fa00eff7059860e6684c35e985fc286712e99817
quan <minhquanbg925@gmail.com>
2023-09-07 21:58:55 +0700

    kieudulieuParse

--- C#/test1.cs
+++ C#/test1.cs
@@ -1,20 +1,10 @@
 public class Program{
     private static void Main(){
-        const int a = 1;
-        Console.WriteLine("int: "+a);
-        const long b = 3;
-        Console.WriteLine("long: "+b);
-        const float c = 3;
-        Console.WriteLine("float: "+c);
-        const double d = 4;
-        Console.WriteLine("double: "+d);
-        const decimal e = 5;
-        Console.WriteLine("decimal: "+e);
-        const bool g = false;
-        Console.WriteLine("bool: "+g);
-        const char h = 'b';
-        Console.WriteLine("char: "+h);
-        const string hoten = "Nguyen Minh Quan";
-        Console.WriteLine("string: "+hoten);
+        int a = 10;
+        float b = a;
+        Console.WriteLine("b= "+b);
+        string str = "123";
+        int c = int.Parse(str);
+        Console.WriteLine("c= "+c);
     }
 }
