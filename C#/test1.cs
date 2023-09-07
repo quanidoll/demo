99a2735871c6958ef3ef6e22405cc955e3e00de1
quan <minhquanbg925@gmail.com>
2023-09-07 21:48:36 +0700

    thuchanhbienso

--- C#/test1.cs
+++ C#/test1.cs
@@ -1,8 +1,20 @@
 public class Program{
     private static void Main(){
-        const string hoTen = "nguyen minh quan";
-        Console.WriteLine("Họ tên: "+hoTen);
-        const int namLamViec = 8;
-        Console.WriteLine("Năm làm việc: "+namLamViec);
+        const int a = 1;
+        Console.WriteLine("int: "+a);
+        const long b = 3;
+        Console.WriteLine("long: "+b);
+        const float c = 3;
+        Console.WriteLine("float: "+c);
+        const double d = 4;
+        Console.WriteLine("double: "+d);
+        const decimal e = 5;
+        Console.WriteLine("decimal: "+e);
+        const bool g = false;
+        Console.WriteLine("bool: "+g);
+        const char h = 'b';
+        Console.WriteLine("char: "+h);
+        const string hoten = "Nguyen Minh Quan";
+        Console.WriteLine("string: "+hoten);
     }
 }
