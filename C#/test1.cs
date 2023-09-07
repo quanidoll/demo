e71b39af22a160d565eb245b41267093f8610dae
quan <minhquanbg925@gmail.com>
2023-09-07 21:41:38 +0700

    bienso

---
+++ C#/test1.cs
@@ -0,0 +1,8 @@
+public class Program{
+    private static void Main(){
+        const string hoTen = "nguyen minh quan";
+        Console.WriteLine("Họ tên: "+hoTen);
+        const int namLamViec = 8;
+        Console.WriteLine("Năm làm việc: "+namLamViec);
+    }
+}
