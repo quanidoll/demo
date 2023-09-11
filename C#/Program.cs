369e4103aaec45e88a5fc130b7e2d9e16991de0c
quan <minhquanbg925@gmail.com>
2023-09-11 13:22:07 +0700

    for

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,17 +1,7 @@
 public class Program{
     private static void Main(){
-        int day = 2;
-        switch(day){
-            case 1 : System.Console.WriteLine("Chủ nhật ");break;
-            case 2 : System.Console.WriteLine("Thứ 2");break;
-            case 3 : System.Console.WriteLine("Thứ 3");break;
-            case 4 : System.Console.WriteLine("Thứ 4");break;
-            case 5 : System.Console.WriteLine("Thứ 5");break;
-            case 6 : System.Console.WriteLine("Thứ 6");break;
-            case 7 : System.Console.WriteLine("Thứ 7");break;
-            default :
-            System.Console.WriteLine("Không phải ngày trong tuần");
-            break;
-        }
+      for(int i = 0 ; i < 20 ; i++){
+        System.Console.WriteLine("Vòng lặp thứ: {0}",i);
+      }
     }
 }
