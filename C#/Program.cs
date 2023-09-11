90c079a5579e336de5736773e0992a943ada481e
quan <minhquanbg925@gmail.com>
2023-09-11 13:18:34 +0700

    switch case

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,20 +1,17 @@
 public class Program{
     private static void Main(){
-       float a = 10;
-       if(a>=8){
-        System.Console.WriteLine("Học lực giỏi");
-       }
-       else if(a>=6.5){
-        System.Console.WriteLine("Học lực khá");
-       }
-       else if(a>=5){
-        System.Console.WriteLine("Học lực trung bình");
-       }
-       else if(a>=3.5){
-        System.Console.WriteLine("học lực yếu");
-       }
-       else {
-        System.Console.WriteLine("học lực kém");
-       }
+        int day = 2;
+        switch(day){
+            case 1 : System.Console.WriteLine("Chủ nhật ");break;
+            case 2 : System.Console.WriteLine("Thứ 2");break;
+            case 3 : System.Console.WriteLine("Thứ 3");break;
+            case 4 : System.Console.WriteLine("Thứ 4");break;
+            case 5 : System.Console.WriteLine("Thứ 5");break;
+            case 6 : System.Console.WriteLine("Thứ 6");break;
+            case 7 : System.Console.WriteLine("Thứ 7");break;
+            default :
+            System.Console.WriteLine("Không phải ngày trong tuần");
+            break;
+        }
     }
 }
