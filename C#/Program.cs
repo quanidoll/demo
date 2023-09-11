aeadcb9d0da355dd968cc33aecdd8f1954833afc
quan <minhquanbg925@gmail.com>
2023-09-11 12:42:21 +0700

    toan tu so hoc

---
+++ C#/Program.cs
@@ -0,0 +1,21 @@
+public class Program{
+    private static void Main(){
+        Console.WriteLine("Các kiểu dữ liệu trong C#: ");
+        const int a = 123;
+        Console.WriteLine("kiểu dữ liệu int: "+a);
+        const long b = 321;
+        Console.WriteLine("kiểu dữ liệu long: "+b);
+        const float c = -123;
+        Console.WriteLine("kiểu dữ liệu float: "+c);
+        const double d = -123456;
+        Console.WriteLine("kiểu dữ liệu double: "+d);
+        const decimal e = -321;
+        Console.WriteLine("kiểu dữ liệu decimal: "+e);
+        const bool g = false;
+        Console.WriteLine("Kiểu dữ liệu bool: "+g);
+        const char h = 'h';
+        Console.WriteLine("kiểu dữ liệu char: "+h);
+        const string f = "Nguyễn Minh Quân";
+        Console.WriteLine("kiểu dữ liệu string: "+f);
+    }
+}
--- C#/test1.cs
+++
@@ -1,9 +0,0 @@
-public class Program{
-    private static void Main(){
-        string str = "123";
-        int ketQua;
-        bool kiemTra = false;
-        kiemTra = int.TryParse(str,out(ketQua));
-        Console.WriteLine("Kết quả= "+ketQua);
-    }
-}
