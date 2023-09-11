dd2a3c14c7fc34c0d5b854e8971c6893b1b48d81
quan <minhquanbg925@gmail.com>
2023-09-11 13:12:47 +0700

    if else

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,25 +1,20 @@
 public class Program{
     private static void Main(){
-       Console.WriteLine("Nhập vào 2 số cần tính: ");
-       Console.WriteLine("Nhập vào 2 số cần so sánh: ");
-       int a,b;
-       System.Console.WriteLine("a = ");
-       a = Convert.ToInt32(Console.ReadLine());
-       System.Console.WriteLine("b = ");
-       b = Convert.ToInt32(Console.ReadLine());
-       System.Console.WriteLine("Tổng của 2 số: {0} + {1} = {2}", a, b, a+b);
-       System.Console.WriteLine("Hiệu của 2 số: {0} - {1} = {2}", a, b, a-b);
-       System.Console.WriteLine("Thương của 2 số: {0} / {1} = {2}", a, b, a/b);
-       System.Console.WriteLine("Tích của 2 số: {0} * {1} = {2}", a, b, a*b);
-       System.Console.WriteLine("Số dư của 2 số: {0} % {1} = {2}", a, b, (a-b *(a/b)));
-       if (a == b) {
-        System.Console.WriteLine("Hai số bằng nhau");
+       float a = 10;
+       if(a>=8){
+        System.Console.WriteLine("Học lực giỏi");
        }
-       if (a > b){
-        System.Console.WriteLine("Số a lớn hơn số b");
+       else if(a>=6.5){
+        System.Console.WriteLine("Học lực khá");
        }
-       if (a < b){
-        System.Console.WriteLine("Số a bé hơn số b");
+       else if(a>=5){
+        System.Console.WriteLine("Học lực trung bình");
+       }
+       else if(a>=3.5){
+        System.Console.WriteLine("học lực yếu");
+       }
+       else {
+        System.Console.WriteLine("học lực kém");
        }
     }
 }
