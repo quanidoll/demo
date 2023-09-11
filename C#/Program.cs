b7f675f1618c14741623ff7c9b24d85486754c7c
quan <minhquanbg925@gmail.com>
2023-09-11 13:27:18 +0700

    do while

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,8 +1,9 @@
 public class Program{
     private static void Main(){
       int a = 1;
-      while(a<10){
+      do{
         System.Console.WriteLine("Vòng lặp thứ: {0}",a);a++;
       }
+      while(a<10);
     }
 }
